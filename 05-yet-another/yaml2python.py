e1b56f7908d7ad353a6fe3cc23ad8ba53809fede
Jerzy Góra <j.gora89@gmail.com>
2018-12-02 17:33:16 +0100

    changes to yaml parser

--- 05-yet-another/yaml2python.py
+++ 05-yet-another/yaml2python.py
@@ -8,16 +8,13 @@ import pprint
 if sys.argv[1] in ['-h', '--help']:
     print('USAGE: ./yaml2python.py [YAML_FILE_PATH]')
 
-# LOAD THE YAML FILE
-f = open(sys.argv[1])
-
 # PRINT THE ORIGINAL STRUCTURE TO THE TERMINAL
 print('*** ORIGINAL YAML FILE ***\n')
-sys.stdout.write(f.read())
+sys.stdout.write(open(sys.argv[1],'r').read())
 
 # FEW BLANK LINES FOR VISIBILITY
 print('\n') * 3
 
 # PRINT THE PYTHON INTERPRETATION OF THE FILE TO THE TERMINAL
 print('*** PYTHON INTERPRETATION ***\n')
-pprint.pprint(yaml.load(f))
+pprint.pprint(yaml.load(open(sys.argv[1])))
