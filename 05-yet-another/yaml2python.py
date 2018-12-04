6d3ab5b0b42c32d6f29ba095a314afc53dfac8b0
Jerzy Góra <j.gora89@gmail.com>
2018-12-04 23:10:18 +0100

    changes to yaml2python

--- 05-yet-another/yaml2python.py
+++ 05-yet-another/yaml2python.py
@@ -2,9 +2,24 @@
 
 import argparse
 import json
-import pprint
-import sys
 import yaml
+
+from pprint import pformat
+from pygments import highlight
+from pygments.formatters import Terminal256Formatter
+from pygments.lexers import PyhthonLexer
+from pygments.lexers.data import JsonLexer, YamlLexer
+
+STYLE='rtt'
+
+def pprint_python(obj):
+    print(highlight(pformat(obj), PyhthonLexer(), Terminal256Formatter(style=STYLE)))
+
+def pprint_json(obj):
+    print(highlight(obj, JsonLexer(), Terminal256Formatter(style=STYLE)))
+
+def pprint_yaml(obj):
+    print(highlight(obj, YamlLexer(), Terminal256Formatter(style=STYLE)))
 
 # PARSE ARGUMENTS
 parser = argparse.ArgumentParser()
@@ -12,19 +27,19 @@ parser.add_argument('-f', '--file', help="path to a YAML file", type=str, defaul
 args = parser.parse_args()
 
 # PRINT THE ORIGINAL STRUCTURE TO THE TERMINAL
-print('*** ORIGINAL YAML FILE ***\n')
-sys.stdout.write(open(args.file,'r').read())
-
-# FEW BLANK LINES FOR VISIBILITY
-print('\n') * 3
+print('\n============================')
+print('***  ORIGINAL YAML FILE  ***')
+print('============================\n')
+pprint_yaml(open(args.file,'r').read())
 
 # PRINT THE JSON EQUIVALENT
-print('*** JSON FILE EQUIVALENT ***\n')
-print(json.dumps(yaml.load(open(args.file)), indent=4, sort_keys=True))
-
-# FEW BLANK LINES FOR VISIBILITY
-print('\n') * 3
+print('\n============================')
+print('*** JSON FILE EQUIVALENT ***')
+print('============================\n')
+pprint_json(json.dumps(yaml.load(open(args.file)), indent=4, sort_keys=True))
 
 # PRINT THE PYTHON INTERPRETATION OF THE FILE TO THE TERMINAL
-print('*** PYTHON INTERPRETATION ***\n')
-pprint.pprint(yaml.load(open(args.file)))
+print('\n=============================')
+print('*** PYTHON INTERPRETATION ***')
+print('=============================\n')
+pprint_python(yaml.load(open(args.file)))
