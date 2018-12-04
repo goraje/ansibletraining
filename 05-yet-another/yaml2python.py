13e63a21788511d5f531fad4e823f978d82b2129
Jerzy Góra <j.gora89@gmail.com>
2018-12-04 22:17:49 +0100

    changes to yaml2python

--- 05-yet-another/yaml2python.py
+++ 05-yet-another/yaml2python.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python
 
-import yaml
-import pprint
 import argparse
+import json
+import pprint
+import sys
+import yaml
 
 # PARSE ARGUMENTS
 parser = argparse.ArgumentParser()
-parser.add_argument('-f', '--file', help="path to aYAML file", type=str, default='template.yml')
+parser.add_argument('-f', '--file', help="path to a YAML file", type=str, default='template.yml')
 args = parser.parse_args()
 
 # PRINT THE ORIGINAL STRUCTURE TO THE TERMINAL
 print('*** ORIGINAL YAML FILE ***\n')
 sys.stdout.write(open(args.file,'r').read())
+
+# FEW BLANK LINES FOR VISIBILITY
+print('\n') * 3
+
+# PRINT THE JSON EQUIVALENT
+print('*** JSON FILE EQUIVALENT ***\n')
+print(json.dumps(yaml.load(open(args.file)), indent=4, sort_keys=True))
 
 # FEW BLANK LINES FOR VISIBILITY
 print('\n') * 3
