6f8b2b637f4c028a82866835731434f91f60fab1
Jerzy Góra <j.gora89@gmail.com>
2018-12-04 22:14:05 +0100

    changes to yaml2python

--- 05-yet-another/yaml2python.py
+++ 05-yet-another/yaml2python.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python
 
-import sys
 import yaml
 import pprint
+import argparse
 
-# SHOW USAGE INFO
-if sys.argv[1] in ['-h', '--help']:
-    print('USAGE: ./yaml2python.py [YAML_FILE_PATH]')
+# PARSE ARGUMENTS
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', '--file', help="path to aYAML file", type=str, default='template.yml')
+args = parser.parse_args()
 
 # PRINT THE ORIGINAL STRUCTURE TO THE TERMINAL
 print('*** ORIGINAL YAML FILE ***\n')
-sys.stdout.write(open(sys.argv[1],'r').read())
+sys.stdout.write(open(args.file,'r').read())
 
 # FEW BLANK LINES FOR VISIBILITY
 print('\n') * 3
 
 # PRINT THE PYTHON INTERPRETATION OF THE FILE TO THE TERMINAL
 print('*** PYTHON INTERPRETATION ***\n')
-pprint.pprint(yaml.load(open(sys.argv[1])))
+pprint.pprint(yaml.load(open(args.file)))
